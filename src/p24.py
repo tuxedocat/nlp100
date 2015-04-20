7e0299b9cf52775935d2463d3afbb4e841c7dc4d
tuxedocat <yusawai.null@gmail.com>
2015-04-21 00:10:03 +0900

    updated p24

--- src/p24.py
+++ src/p24.py
@@ -18,7 +18,7 @@ filepath = "../dat/jawiki-country.json.gz"
 
 def searchmedia(line):
     """Extract media [[File:<filename>...]]"""
-    media = re.compile(r"(?<=\[\[File:).+?\.[A-Za-z]{3}")
+    media = re.compile(r"(?<=\[\[File:).+?\.[A-Za-z0-9]{3}") # non-greedy search
     return media.search(line)
 
 if __name__ == '__main__':
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             _j = json.loads(line.decode("utf-8"))
             j[_j["title"]] = _j["text"]
     text = j["イギリス"]
-    
+
     for m in (searchmedia(line) for line in text.split("\n")):
         if m:
             print(m.group())
